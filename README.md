87ce5d035c0a08c71f97ab320167c1f6d712716e
Enitan Odupitan <odupitanenitan@gmail.com>
2023-10-14 17:50:52 +0300

    adds content to readme file

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# pokedex_JS
+# Project title
+
+PokeDex
+The Pokedex Application is a web-based tool designed using HTML, CSS, and JavaScript, with real-time data fetched from an online API. This user-friendly application serves as a digital encyclopedia for Pokémon enthusiasts, allowing users to search for various species of pokemon by ID, name, types and generation
+
+## Technologies used
+
+Built with:
+
+- HTML
+- JS
+- CSS
+
+
+## Setup and usage
+
+- At the web app front page, select from the drop down select tag to choose which geenration of 
+pokemon species that needs to be displayed.
+
+- Once the generation displayed, one can filter the results by name, types. 
+
+## Screenshot
+![Alt text](<images/Screenshot 2023-10-14 at 17.46.28.png>)
+## Sources
+
+- [API_source](https://pokeapi.co/api/v2/pokemon)
+
+
+## Author
+
+Enitan Odupitan
+
---
+++ main.js
@@ -0,0 +1,109 @@
+const generationSelect = document.querySelector(".generationSelect");
+const searchTab = document.querySelector('.searchBar');
+const typeSelect = document.querySelector("#typeSelect")
+
+
+let generations = [
+    { limit: 151, offset: 0 },
+    { limit: 100, offset: 151 },
+    { limit: 135, offset: 251 },
+    { limit: 107, offset: 386 },
+    { limit: 156, offset: 493 },
+    { limit: 72, offset: 649 },
+    { limit: 88, offset: 721 },
+    { limit: 96, offset: 809 },
+    { limit: 110, offset: 905 },
+];
+
+const infoList = (data) => {
+    document.querySelector('.pokemonsImage').innerHTML = data.map((item) => {
+        return `<div class = 'pokeCards'>
+        <img src="${item.sprites.other["official-artwork"].front_default}"/>
+        <p>Name: ${item.name} <br>Type: ${item.types[0].type.name} </p></div>`
+    }).join('')
+
+}
+
+const apiLink = 'https://pokeapi.co/api/v2/pokemon';
+let pokeData = [];
+
+generationSelect.addEventListener('change', () => {
+    const selectedGeneration = generationSelect.value;
+    const generationData = generations[selectedGeneration];
+
+    if (selectedGeneration === "0") {
+        fetchAllpokemon();
+    }
+    else {
+        fetchPokemonByGeneration(generationData.limit, generationData.offset)
+    }
+});
+
+function fetchAllpokemon() {
+    fetch(`${apiLink}?limit=${limit}&offset=${offset}`)
+        .then((response) => response.json())
+        .then((json) => {
+            const fetches = json.results.map((i)=> {
+                return fetch(i.url).then((response) => response.json)
+            });
+
+            Promise.all(fetches).then((response) => {
+                pokeData = response;
+                infoList(pokeData)
+            })
+        })
+}
+
+function fetchPokemonByGeneration(limit, offset) {
+    fetch(`${apiLink}?limit=${limit}&offset=${offset}`)
+        .then((response) => response.json())
+        .then((json) => {
+            const fetches = json.results.map((i)=> {
+                return fetch(i.url).then((response) => response.json())
+            });
+
+            Promise.all(fetches).then((response) => {
+                pokeData = response;
+                infoList(pokeData);
+            });
+        });
+
+}
+
+
+typeSelect.addEventListener('change', ()=>{
+    const selectedType = typeSelect.value;
+
+    if (selectedType === "all") {
+        infoList(pokeData)
+    } else {
+        filterPokemonByType(selectedType)
+    }
+})
+
+function filterPokemonByType(selectedType) {
+    if (Array.isArray(pokeData)) {
+        const typeData = pokeData.filter((item) => {
+            return item.types.some((type) => type.type.name === selectedType);
+        });
+        infoList(typeData);
+    }
+}
+
+
+function searchPokemon() {
+    let input = searchTab.value.toLowerCase();
+    let cards = document.querySelectorAll('.cards');
+
+    cards.forEach(card => {
+        let cardText = card.textContent.toLowerCase();
+        if(cardText.includes(input)) {
+            card.style.display = "list-item"
+        } else {
+            card.style.display = "none"
+        }
+    })
+}
+
+
+searchTab.addEventListener('keyup', searchPokemon);
